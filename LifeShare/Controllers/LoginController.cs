29beeccbecf84be8d96b20d39c174437107c654d
Vinicius Starck <80191688+Vini-Starck@users.noreply.github.com>
2024-11-05 20:15:18 -0300

    Update LoginController.cs

--- LifeShare/Controllers/LoginController.cs
+++ LifeShare/Controllers/LoginController.cs
@@ -9,29 +9,17 @@ namespace LifeShare.Controllers
 {
     public class LoginController : Controller
     {
-
-        Login AdminModel = new Login();
-
         [HttpPost]
         public IActionResult Autenticar(Login Admin)
         {
-            if(Admin.Email != null && Admin.Senha != null)
-            {
-                if(Admin.Email.Equals(AdminModel.Email) && Admin.Senha.Equals(AdminModel.Senha))
-                {
-                    return Redirect("/Empresa/Index");
-                }
-            }
-                TempData["msg"] = "Login ou senha incorretos!";
-                return View();
+            // Redireciona diretamente para a próxima página, sem validação
+            return Redirect("/Empresa/Index");
         }
 
-       [HttpGet]
+        [HttpGet]
         public IActionResult Autenticar()
         {
             return View();
         }
-
-    
     }
 }
